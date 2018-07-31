363506934e3457baa48e150d5f17eaf8197d2a46
Bartosz Selwesiuk <bartek@leancode.pl>
2018-07-31 10:06:17 +0200

    Add missing error's prototype

--- packages/LoginManager/src/CannotRefreshToken.ts
+++ packages/LoginManager/src/CannotRefreshToken.ts
@@ -1,1 +1,6 @@
-export class CannotRefreshToken extends Error { }
+export class CannotRefreshToken extends Error {
+    constructor(m: string) {
+        super(m);
+        Object.setPrototypeOf(this, CannotRefreshToken.prototype);
+    }
+}
