d1d4fa43f52e57613e021fcbeb051601f985fbc5
Mateusz Wojtczak <mateusz@leancode.pl>
2018-10-01 15:36:58 +0200

    Remove sync flag from token storage

--- packages/LoginManager/src/TokenStorage.ts
+++ packages/LoginManager/src/TokenStorage.ts
@@ -11,12 +11,10 @@ export interface SyncTokenStorage {
     getToken(): Token | null;
     storeToken(token: Token): void;
     resetToken(): void;
-    sync: true;
 }
 
 export interface AsyncTokenStorage {
     getToken(): Promise<Token | null>;
     storeToken(token: Token): Promise<void>;
     resetToken(): Promise<void>;
-    sync: false;
 }
