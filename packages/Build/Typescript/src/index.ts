3c3024eee85e9f8c2e0ff30b42733d2fb1daaa94
Sebastian Konowrocki <sebastian.konowrocki@leancode.pl>
2019-11-06 10:37:54 +0100

    Extend typescript default configuration

--- packages/Build/Typescript/src/index.ts
+++ packages/Build/Typescript/src/index.ts
@@ -44,7 +44,19 @@ export default function typescript<TInCtx extends EnvironmentContext>(
             ],
         ];
 
-        const babelPlugins: any[] = ["@babel/plugin-proposal-class-properties", "@babel/plugin-syntax-dynamic-import"];
+        const babelPlugins: any[] = [
+            ["@babel/plugin-proposal-decorators", { legacy: true }],
+            "@babel/plugin-proposal-nullish-coalescing-operator",
+            "@babel/plugin-proposal-optional-chaining",
+            "@babel/plugin-proposal-class-properties",
+            "@babel/plugin-syntax-dynamic-import",
+            [
+                "const-enum",
+                {
+                    transform: "removeConst",
+                },
+            ],
+        ];
 
         ctx.config.module.rules.push(
             {
