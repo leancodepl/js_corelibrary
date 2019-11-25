50ce8647c39860b48fa6905ba4a01e7f9fece128
Tomasz Laskowski <tomasz.laskowski@leancode.pl>
2019-11-25 12:52:10 +0100

    Use loaders conditionally in build-sass

--- packages/Build/Sass/src/index.ts
+++ packages/Build/Sass/src/index.ts
@@ -1,6 +1,7 @@
 import { Configure } from "@leancode/build-base/configure";
 import { EnvironmentContext } from "@leancode/build-base/environment";
 import { CssContext } from "@leancode/build-css";
+import webpack from "webpack";
 
 export default function sass<TInCtx extends CssContext & EnvironmentContext>(
     globalResources: string[],
@@ -8,21 +9,42 @@ export default function sass<TInCtx extends CssContext & EnvironmentContext>(
     return ctx => {
         ctx.config.module = ctx.config.module || { rules: [] };
 
+        const additionalLoaders: webpack.Loader[] = ["sass-loader"];
+
+        if (globalResources.length) {
+            additionalLoaders.push({
+                loader: "sass-resources-loader",
+                options: {
+                    sourceMap: !ctx.isProduction,
+                    resources: globalResources,
+                },
+            });
+        }
+
         ctx.config.module.rules.push({
             test: /\.(sass|scss)$/,
-            use: [
-                ...ctx.styleLoader({
-                    additionalLoaders: 2,
-                    dontUseTypescript: ctx.isProduction, // there is a problem with verify in node_modules
-                }),
-                "sass-loader",
+            oneOf: [
                 {
-                    loader: "sass-resources-loader",
-                    options: {
-                        sourceMap: !ctx.isProduction,
-                        resources: globalResources,
-                    },
+                    resourceQuery: /global/,
+                    use: [
+                        ...ctx.styleLoader({
+                            additionalLoaders: additionalLoaders.length,
+                            dontUseCssModules: true,
+                            dontUseTypescript: ctx.isProduction,
+                        }),
+                        ...additionalLoaders,
+                    ]
                 },
+                {
+                    use: [
+                        ...ctx.styleLoader({
+                            additionalLoaders: additionalLoaders.length,
+                            dontUseCssModules: false,
+                            dontUseTypescript: ctx.isProduction,
+                        }),
+                        ...additionalLoaders,
+                    ]
+                }
             ],
         });
 
