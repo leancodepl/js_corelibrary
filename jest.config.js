f73841f3a022a32d4334a8f8bd1148dac6fb761b
Sebastian Konowrocki <sebastian.konowrocki@leancode.pl>
2020-02-11 17:58:14 +0100

    chore(tests): tests setup continuation

--- jest.config.js
+++ jest.config.js
@@ -1,22 +1,30 @@
 module.exports = {
-  reporters: [
-    "default",
-    ["jest-junit", { outputName: "test-results/report.xml" }]
-  ],
-  projects: [
-    {
-      displayName: "Test",
-      transform: {
-        "^.+\\.tsx?$": "ts-jest"
-      },
-      testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$",
-      transformIgnorePatterns: ["<rootDir>/node_modules/"],
-      moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"]
-    },
-    {
-      displayName: "ESLint",
-      runner: "jest-runner-eslint",
-      testMatch: ["<rootDir>/packages/**/*.{ts,tsx}"]
-    }
-  ]
+    reporters: ["default", ["jest-junit", { outputName: "test-results/report.xml" }]],
+
+    projects: [
+        {
+            displayName: "Test",
+            transform: {
+                "^.+\\.tsx?$": "ts-jest",
+            },
+            // roots: ["<rootDir>/packages/LoginManager"],
+            globals: {
+                "ts-jest": {
+                    tsConfig: {
+                        lib: ["dom", "es6"],
+                    },
+                },
+            },
+            testRegex: "(/__tests__/.*\\.spec)\\.(jsx?|tsx?)$",
+            transformIgnorePatterns: ["<rootDir>/node_modules/"],
+            moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json"],
+        },
+        // {
+        //     displayName: "ESLint",
+        //     runner: "jest-runner-eslint",
+        //     roots: ["<rootDir>packages"],
+        //     testMatch: ["<rootDir>/packages/**/*.{ts,tsx}"],
+        //     testPathIgnorePatterns: ["/node_modules", ".*/lib/.*"],
+        // },
+    ],
 };
--- packages/CqrsClient/__tests__/test.spec.ts
+++ packages/CqrsClient/__tests__/test.spec.ts
@@ -1,5 +1,5 @@
 describe("test", () => {
     it("should add correctly", async () => {
-      expect(2 + 2).toBe(4);
+        expect(2 + 2).toBe(4);
     });
 });
