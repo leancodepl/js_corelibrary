ff59786bf34e6cae4b9ae391a6ead6011f5672f3
Sebastian Konowrocki <sebastian.konowrocki@leancode.pl>
2020-02-14 17:42:19 +0100

    chore(ci): use npm install instead of ci

--- rollup.config.js
+++ rollup.config.js
@@ -8,27 +8,24 @@ const OUTPUT_DIR = path.join(PACKAGE_ROOT_PATH, "lib");
 const PKG_JSON = require(path.join(PACKAGE_ROOT_PATH, "package.json"));
 
 const formats = [
-  { dist: "umd", ts: "es5" },
-  { dist: "es", ts: "esnext" }
+    { dist: "umd", ts: "es5" },
+    { dist: "es", ts: "esnext" },
 ];
 
 export default formats.map(format => ({
-  plugins: [
-    typescript({
-      tsconfig: TSCONFIG_FILE,
-      tsconfigOverride: { compilerOptions: { target: format.ts } }
-    })
-  ],
-  input: INPUT_FILE,
-  external: [
-    ...Object.keys(PKG_JSON.dependencies || {}),
-    ...Object.keys(PKG_JSON.peerDependencies || {})
-  ],
-  output: {
-    file: path.join(OUTPUT_DIR, `index.${format.ts}.js`),
-    format: format.dist,
-    sourcemap: true,
-    name: PKG_JSON.name,
-    exports: "named"
-  }
+    plugins: [
+        typescript({
+            tsconfig: TSCONFIG_FILE,
+            tsconfigOverride: { compilerOptions: { target: format.ts } },
+        }),
+    ],
+    input: INPUT_FILE,
+    external: [...Object.keys(PKG_JSON.dependencies || {}), ...Object.keys(PKG_JSON.peerDependencies || {})],
+    output: {
+        file: path.join(OUTPUT_DIR, `index.${format.ts}.js`),
+        format: format.dist,
+        sourcemap: true,
+        name: PKG_JSON.name,
+        exports: "named",
+    },
 }));
