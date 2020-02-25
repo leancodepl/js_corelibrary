393dc37fa569db1efbf515c7b714aabbb7df025f
Sebastian Konowrocki <sebastian.konowrocki@leancode.pl>
2020-02-25 17:28:06 +0100

    feat(i18n): implement translation factory

--- packages/I18n/src/index.tsx
+++ packages/I18n/src/index.tsx
@@ -1,36 +1,120 @@
-import React, { createElement, ReactNode } from "react";
-import { FormattedMessage } from "react-intl";
+import React, { ReactNode, useEffect, useRef, useState } from "react";
+import { createIntl, createIntlCache, FormattedMessage, IntlShape, RawIntlProvider, useIntl } from "react-intl";
+
 type FormattedMessageProps = import("react-intl/dist/components/message").Props;
 
-type Pl = typeof import("./locales/pl.json");
-type En = typeof import("./locales/en.json");
+const languageChangedEvent = "LanguageChanged";
 
-// type LanguageEquity = AssertNever<UnionEqual<keyof Pl, keyof En>>;
+export function getDefaultLanguage<TSupportedLanguage extends string>(
+    supportedLanguages: TSupportedLanguage[],
+    fallbackLanguage?: TSupportedLanguage,
+): TSupportedLanguage;
+export function getDefaultLanguage(): string;
+export function getDefaultLanguage(supportedLanguages?: string[], fallbackLanguage?: string) {
+    const navigatorLanguage = navigator.language.substr(0, 2).toLowerCase();
 
-export type Term = keyof Pl | keyof En;
+    if (supportedLanguages) {
+        if (supportedLanguages.includes(navigatorLanguage)) {
+            return navigatorLanguage;
+        }
 
-export const supportedLanguages = ["pl", "en"] as const;
-export type SupportedLanguages = typeof supportedLanguages[number];
+        if (fallbackLanguage) {
+            return fallbackLanguage;
+        }
 
-export default function mkI18n<TSupportedLanguages extends string>(
-    locales: {
-        [TSupportedLanguage in TSupportedLanguages]: any;
-    },
-) {
-    return {
-        Localize: (props: FormattedMessageProps & { id: Term }) => <FormattedMessage {...props} />,
-        Provider: 
-    };
+        throw new Error(`Navigator language (${navigatorLanguage}) is not supported`);
+    }
+
+    return navigatorLanguage;
 }
 
-const defaultAllowedRichTextFlags = ["ul", "li", "p", "b", "i"] as (keyof JSX.IntrinsicElements)[];
-export const renderDefaultRichTextTags = (allowedTags: (keyof JSX.IntrinsicElements)[] = defaultAllowedRichTextFlags) =>
-    Object.fromEntries(
-        allowedTags.map(tag => [
-            tag,
-            (() => {
-                const renderTag = (...children: ReactNode[]) => createElement(tag, {}, ...children);
-                return renderTag;
-            })(),
-        ]),
-    );
+declare global {
+    interface Window {
+        currentLanguage?: string;
+    }
+}
+
+export default function mkI18n<TSupportedLanguage extends string, TTerm extends string>(
+    locales: {
+        [TKey in TSupportedLanguage]: () => Promise<Record<TTerm, string>>;
+    },
+    defaultLanguage: TSupportedLanguage,
+) {
+    window.currentLanguage = defaultLanguage;
+
+    const cache = createIntlCache();
+
+    const messagesCache: Partial<{ [TKey in TSupportedLanguage]: Record<TTerm, string> }> = {};
+
+    return {
+        Localize: (props: FormattedMessageProps & { id: TTerm }) => <FormattedMessage {...props} />,
+        useIntl: useIntl as () => IntlShape & { messages: TTerm[] },
+        Provider({ children }: { children?: ReactNode }) {
+            const [currentLanguage, setCurrentLanguage] = useState<TSupportedLanguage>(
+                () => window.currentLanguage! as TSupportedLanguage,
+            );
+
+            const currentLanguageRef = useRef(currentLanguage);
+
+            const [intl, setIntl] = useState(() =>
+                createIntl(
+                    {
+                        locale: currentLanguage!,
+                        messages: messagesCache[currentLanguage!],
+                    },
+                    cache,
+                ),
+            );
+
+            useEffect(() => {
+                const handler = () => {
+                    setCurrentLanguage(window.currentLanguage as TSupportedLanguage);
+                    currentLanguageRef.current = window.currentLanguage as TSupportedLanguage;
+                };
+
+                window.addEventListener(languageChangedEvent, handler);
+
+                return () => window.removeEventListener(languageChangedEvent, handler);
+            }, []);
+
+            useEffect(() => {
+                const cachedMessages = messagesCache[currentLanguage];
+
+                if (cachedMessages) {
+                    setIntl(
+                        createIntl(
+                            {
+                                locale: currentLanguage!,
+                                messages: cachedMessages,
+                            },
+                            cache,
+                        ),
+                    );
+                }
+                (async () => {
+                    const messages = await locales[currentLanguage]();
+
+                    messagesCache[currentLanguage] = messages;
+
+                    if (currentLanguageRef.current === currentLanguage) {
+                        setIntl(
+                            createIntl(
+                                {
+                                    locale: currentLanguage!,
+                                    messages,
+                                },
+                                cache,
+                            ),
+                        );
+                    }
+                })();
+            }, [currentLanguage]);
+
+            return <RawIntlProvider value={intl}>{children}</RawIntlProvider>;
+        },
+        changeLocale(language: TSupportedLanguage) {
+            window.currentLanguage = language;
+            window.dispatchEvent(new Event(languageChangedEvent));
+        },
+    };
+}
