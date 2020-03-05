9c7539bd7703c4695ccc946a51c81d1367b1fb86
Sebastian Konowrocki <sebastian.konowrocki@leancode.pl>
2020-03-05 17:21:48 +0100

    chore: switch to yarn

---
+++ tools/rollup/bin/index.js
@@ -0,0 +1,55 @@
+#!/usr/bin/env node
+
+const typescript = require("rollup-plugin-typescript2");
+const clear = require("rollup-plugin-clear");
+const path = require("path");
+const fs = require("fs");
+const rollup = require("rollup");
+
+const formats = [
+    { format: "umd", target: "es5", name: undefined },
+    { format: "esm", target: "esnext", name: "esm" },
+];
+
+const packageRootPath = process.cwd();
+let inputFile = path.join(packageRootPath, "src/index.tsx");
+if (!fs.existsSync(inputFile)) {
+    inputFile = path.join(packageRootPath, "src/index.ts");
+}
+const tsconfigFile = path.join(packageRootPath, "tsconfig.json");
+const outputDir = path.join(packageRootPath, "lib");
+const packageJsonFile = require(path.join(packageRootPath, "package.json"));
+
+/** @type {[rollup.RollupOptions, rollup.OutputOptions][]} */
+
+const config = formats.map(format => ({
+    plugins: [
+        typescript({
+            tsconfig: tsconfigFile,
+            tsconfigOverride: { compilerOptions: { target: format.target } },
+        }),
+        clear({
+            targets: ["lib"],
+        }),
+    ],
+    input: inputFile,
+    external: [
+        ...Object.keys(packageJsonFile.dependencies || {}),
+        ...Object.keys(packageJsonFile.peerDependencies || {}),
+    ],
+    output: {
+        file: path.join(outputDir, `index${format.name ? `.${format.name}` : ""}.js`),
+        format: format.format,
+        sourcemap: true,
+        name: packageJsonFile.name,
+        exports: "named",
+    },
+}));
+
+(async () => {
+    for (const options of config) {
+        const bundle = await rollup.rollup(options);
+
+        await bundle.write(options.output);
+    }
+})();
