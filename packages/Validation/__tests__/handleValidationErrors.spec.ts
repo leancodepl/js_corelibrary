09a3d8d659910083d1cd84d03aac6d949a84d92c
Sebastian Konowrocki <sebastian.konowrocki@leancode.pl>
2020-04-22 12:42:09 +0200

    chore(validation): use spies instead of fakes in tests

--- packages/Validation/__tests__/handleValidationErrors.spec.ts
+++ packages/Validation/__tests__/handleValidationErrors.spec.ts
@@ -1,7 +1,7 @@
 import sinon from "sinon";
 import { handleValidationErrors } from "../src";
 
-const myCommand = {
+const commandUnderTest = {
     ErrorCodes: {
         Error1: 1,
         Error2: 2,
@@ -10,18 +10,17 @@ const myCommand = {
 } as const;
 
 const emptyHandler = () => {};
-type EmptyHandler = typeof emptyHandler;
 
 describe("handleValidationErrors", () => {
     it("does not call any validation handler if there are no errors", () => {
-        const handleError1 = sinon.fake(emptyHandler);
-        const handleError2 = sinon.fake(emptyHandler);
-        const handleError3 = sinon.fake(emptyHandler);
+        const handleError1 = sinon.spy(emptyHandler);
+        const handleError2 = sinon.spy(emptyHandler);
+        const handleError3 = sinon.spy(emptyHandler);
 
-        handleValidationErrors([], myCommand)
-            .handle("Error1", handleError1 as EmptyHandler)
-            .handle("Error2", handleError2 as EmptyHandler)
-            .handle("Error3", handleError3 as EmptyHandler)
+        handleValidationErrors([], commandUnderTest)
+            .handle("Error1", handleError1)
+            .handle("Error2", handleError2)
+            .handle("Error3", handleError3)
             .check();
 
         expect(handleError1.called).toBeFalsy();
@@ -30,10 +29,10 @@ describe("handleValidationErrors", () => {
     });
 
     it("calls single validation handler if there is single error", () => {
-        const handleError1 = sinon.fake();
+        const handleError1 = sinon.spy(emptyHandler);
 
-        handleValidationErrors([{ ErrorCode: 1 }], myCommand)
-            .handle("Error1", handleError1 as EmptyHandler)
+        handleValidationErrors([{ ErrorCode: 1 }], commandUnderTest)
+            .handle("Error1", handleError1)
             .handle("Error2", emptyHandler)
             .handle("Error3", emptyHandler)
             .check();
@@ -42,12 +41,12 @@ describe("handleValidationErrors", () => {
     });
 
     it("calls multiple validation handlers if there are multiple errors", () => {
-        const handleError1 = sinon.fake();
-        const handleError2 = sinon.fake();
+        const handleError1 = sinon.spy(emptyHandler);
+        const handleError2 = sinon.spy(emptyHandler);
 
-        handleValidationErrors([{ ErrorCode: 1 }, { ErrorCode: 2 }], myCommand)
-            .handle("Error1", handleError1 as EmptyHandler)
-            .handle("Error2", handleError2 as EmptyHandler)
+        handleValidationErrors([{ ErrorCode: 1 }, { ErrorCode: 2 }], commandUnderTest)
+            .handle("Error1", handleError1)
+            .handle("Error2", handleError2)
             .handle("Error3", emptyHandler)
             .check();
 
@@ -56,10 +55,10 @@ describe("handleValidationErrors", () => {
     });
 
     it("calls joint validation handler when there is single related error", () => {
-        const handleError1_2 = sinon.fake();
+        const handleError1_2 = sinon.spy(emptyHandler);
 
-        handleValidationErrors([{ ErrorCode: 1 }], myCommand)
-            .handle(["Error1", "Error2"], handleError1_2 as EmptyHandler)
+        handleValidationErrors([{ ErrorCode: 1 }], commandUnderTest)
+            .handle(["Error1", "Error2"], handleError1_2)
             .handle("Error3", emptyHandler)
             .check();
 
@@ -67,10 +66,10 @@ describe("handleValidationErrors", () => {
     });
 
     it("calls joint validation handler once when there are multiple related error", () => {
-        const handleError1_2 = sinon.fake();
+        const handleError1_2 = sinon.spy(emptyHandler);
 
-        handleValidationErrors([{ ErrorCode: 1 }, { ErrorCode: 2 }], myCommand)
-            .handle(["Error1", "Error2"], handleError1_2 as EmptyHandler)
+        handleValidationErrors([{ ErrorCode: 1 }, { ErrorCode: 2 }], commandUnderTest)
+            .handle(["Error1", "Error2"], handleError1_2)
             .handle("Error3", emptyHandler)
             .check();
 
@@ -79,10 +78,10 @@ describe("handleValidationErrors", () => {
     });
 
     it("calls joint validation handler only with first (most important) validation error", () => {
-        const handleError1_2 = sinon.fake();
+        const handleError1_2 = sinon.spy((error: "Error1" | "Error2") => {}); // eslint-disable-line @typescript-eslint/no-unused-vars
 
-        handleValidationErrors([{ ErrorCode: 2 }, { ErrorCode: 1 }], myCommand)
-            .handle(["Error1", "Error2"], handleError1_2 as EmptyHandler)
+        handleValidationErrors([{ ErrorCode: 2 }, { ErrorCode: 1 }], commandUnderTest)
+            .handle(["Error1", "Error2"], handleError1_2)
             .handle("Error3", emptyHandler)
             .check();
 
