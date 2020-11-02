018fae49927f554fd80a3e780e003bf2400b1727
Wojciech Moczydlowski <wojciech.moczydlowski@leancode.pl>
2020-11-02 11:38:44 +0100

    feat: extend eslint rules

--- packages/Build/Base/src/entrypoint.ts
+++ packages/Build/Base/src/entrypoint.ts
@@ -8,23 +8,20 @@ export default function entrypoint<TInCtx extends BaseContext>(
     entrypoint: string,
     appUrl: string,
 ): Configure<TInCtx, TInCtx & AppUrlContext> {
-    return ctx => {
-        return {
-            ...ctx,
-            config: {
-                ...ctx.config,
-                entry: [
-                    ctx.argv.inline && `webpack-dev-server/client?${appUrl}`,
-                    ctx.argv.inline && "webpack/hot/only-dev-server",
-                    entrypoint,
-                ].filter(e => e),
-                resolve: {
-                    ...ctx.config.resolve,
-                    extensions: [...((ctx.config.resolve || {}).extensions || []), ".js"],
-                },
+    return ctx => ({
+        ...ctx,
+        config: {
+            ...ctx.config,
+            entry: [
+                ctx.argv.inline && `webpack-dev-server/client?${appUrl}`,
+                ctx.argv.inline && "webpack/hot/only-dev-server",
+                entrypoint,
+            ].filter(e => e),
+            resolve: {
+                ...ctx.config.resolve,
+                extensions: [...((ctx.config.resolve || {}).extensions || []), ".js"],
             },
-
-            appUrl,
-        };
-    };
+        },
+        appUrl,
+    });
 }
