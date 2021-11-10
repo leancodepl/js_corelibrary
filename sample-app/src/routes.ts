f67355c7e9882270826e13d1ed947f6903d059ab
Sebastian Konowrocki <sebastian.konowrocki@leancode.pl>
2021-11-10 21:55:38 +0100

    feat: update packages

--- sample-app/src/routes.ts
+++ sample-app/src/routes.ts
@@ -5,7 +5,7 @@ import { createRouting, PathParamsFor, segment, arg } from "ts-routes";
 const routes = createRouting({
     home: segment`/`,
     splitComponent: segment`/split-component`,
-    test: segment`test/${arg("test", { optional: true })}`,
+    test: segment`test/${arg("test", { optionality: "optional" })}`,
 } as const);
 
 export default routes;
