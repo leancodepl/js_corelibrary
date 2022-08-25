a3c589924d5fd96806dc481fd2a2c4fe456bd0a6
Katarzyna Grasewicz <katarzyna.grasewicz@leancode.pl>
2022-08-25 16:41:37 +0200

    fix: fromApiTimeSpan test

--- packages/CqrsClients/CustomTypes/Date/Date-fns/__tests__/timeSpan/fromApiTimeSpan.spec.ts
+++ packages/CqrsClients/CustomTypes/Date/Date-fns/__tests__/timeSpan/fromApiTimeSpan.spec.ts
@@ -2,7 +2,7 @@ import { ApiTimeSpan } from "@leancode/api-date";
 import timezoneMock from "timezone-mock";
 import { fromApiTimeSpan } from "../../src";
 
-const expectedNegativeDuration = { years: -0, months: -0, days: -1, hours: -3, minutes: -16, seconds: -50 };
+const expectedNegativeDuration = { days: -1, hours: -3, minutes: -16, seconds: -50 };
 
 describe("fromApiTimeSpan", () => {
     it("handles undefined", () => {
@@ -27,29 +27,9 @@ describe("fromApiTimeSpan", () => {
 
             const timeSpan = fromApiTimeSpan(apiTimeSpan as unknown as ApiTimeSpan);
 
-            const dateFnsDuration: Duration = { years: 0, months: 0, days: 1, hours: 3, minutes: 16, seconds: 50 };
+            const dateFnsDuration: Duration = { days: 1, hours: 3, minutes: 16, seconds: 50 };
 
             expect(timeSpan).toStrictEqual(dateFnsDuration);
-        });
-
-        it("converts api timespan to duration in months", () => {
-            const apiTimeSpan = "30.00:00:00";
-
-            const timeSpan = fromApiTimeSpan(apiTimeSpan as unknown as ApiTimeSpan);
-
-            const timeSpanInMonths = timeSpan.months;
-
-            expect(timeSpanInMonths).toBe(0);
-        });
-
-        it("converts api timespan to duration in months", () => {
-            const apiTimeSpan = "31.00:00:00";
-
-            const timeSpan = fromApiTimeSpan(apiTimeSpan as unknown as ApiTimeSpan);
-
-            const timeSpanInMonths = timeSpan.months;
-
-            expect(timeSpanInMonths).toBe(1);
         });
 
         it("converts api timespan to duration in hours", () => {
@@ -87,29 +67,9 @@ describe("fromApiTimeSpan", () => {
 
             const timeSpan = fromApiTimeSpan(apiTimeSpan as unknown as ApiTimeSpan);
 
-            const dateFnsDuration: Duration = { years: 0, months: 0, days: 1, hours: 3, minutes: 16, seconds: 50 };
+            const dateFnsDuration: Duration = { days: 1, hours: 3, minutes: 16, seconds: 50 };
 
             expect(timeSpan).toStrictEqual(dateFnsDuration);
-        });
-
-        it("converts api timespan to duration in months", () => {
-            const apiTimeSpan = "30.00:00:00";
-
-            const timeSpan = fromApiTimeSpan(apiTimeSpan as unknown as ApiTimeSpan);
-
-            const timeSpanInMonths = timeSpan.months;
-
-            expect(timeSpanInMonths).toBe(0);
-        });
-
-        it("converts api timespan to duration in months", () => {
-            const apiTimeSpan = "31.00:00:00";
-
-            const timeSpan = fromApiTimeSpan(apiTimeSpan as unknown as ApiTimeSpan);
-
-            const timeSpanInMonths = timeSpan.months;
-
-            expect(timeSpanInMonths).toBe(1);
         });
 
         it("converts api timespan to duration in hours", () => {
