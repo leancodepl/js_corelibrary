96ece4560f0828d954167efdd5b64f5873d7d353
Łukasz Komoszyński <lukasz.komoszynski@leancode.pl>
2023-09-01 10:06:19 +0200

    feat: add gtag helper library

---
+++ packages/gtag/src/index.ts
@@ -0,0 +1,1 @@
+export { mkgtag } from "./lib";
---
+++ packages/gtag/src/lib/index.ts
@@ -0,0 +1,21 @@
+type DataLayerArguments<T extends { event: string }> = {
+    /**
+     * Function that will be called after all Tags have fired for the given event
+     *
+     * Will not be called when GTM is not initialized
+     */
+    eventCallback?: (containerId: string) => void;
+    eventTimeout?: number;
+} & T;
+
+declare global {
+    interface Window {
+        dataLayer?: DataLayerArguments<{ event: string }>[];
+    }
+}
+
+export function mkgtag<T extends { event: string }>() {
+    return (dataLayerArguments: DataLayerArguments<T>) => {
+        window.dataLayer?.push(dataLayerArguments);
+    };
+}
