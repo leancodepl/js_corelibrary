f8e3018bf038ac64d62cc9e047ccf96dd7ba47ba
Aleksander Szczepura <al.szczepura@gmail.com>
2023-12-07 13:45:15 +0100

    fix: kratos exports

--- packages/kratos/src/index.ts
+++ packages/kratos/src/index.ts
@@ -5,6 +5,7 @@ export * from "./lib/types/responseError";
 export * from "./lib/types/useHandleFlowError";
 export * from "./lib/sessionManager/baseSessionManager";
 export * from "./lib/types/enums";
+export * from "./lib/types/components";
 export * from "./lib/utils/typeGuards";
 
 export * from "./lib/flows";
