a09dd98f9fa8cf7b92d0fc4246302eae5c0fbd82
Łukasz Garstecki <lukasz.garstecki@leancode.pl>
2023-12-28 11:20:32 +0100

    Fix function name

--- packages/utils/src/lib/enumify.ts
+++ packages/utils/src/lib/enumify.ts
@@ -1,3 +1,3 @@
-export function mkErrors<TError extends string>(errorNames: TError[]) {
+export function enumify<TError extends string>(errorNames: TError[]) {
     return errorNames.reduce((a, v) => ({ ...a, [v]: v }), {}) as { [T in TError]: T }
 }
