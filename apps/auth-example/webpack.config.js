61b1f0e8f7ff273b36ec13eb01c9bb55f4ff7d14
alszczep <al.szczepura@gmail.com>
2024-04-17 12:40:34 +0200

    chore: bump nx and linters packages

--- apps/auth-example/webpack.config.js
+++ apps/auth-example/webpack.config.js
@@ -1,5 +1,6 @@
 const { withReact } = require("@nx/react");
 const { composePlugins, withNx } = require("@nx/webpack");
+const { configurationGeneratorInternal } = require("@nx/webpack/src/generators/configuration/configuration");
 
 // Nx plugins for webpack.
 module.exports = composePlugins(withNx(), withReact(), config => {
@@ -15,6 +16,16 @@ module.exports = composePlugins(withNx(), withReact(), config => {
             },
         },
     };
+
+    // const loader = config.module.rules.find(e => typeof e.loader === "string" && e.loader.includes("swc-loader"))
+    // loader.options.jsc.target = "es2016"
+    // loader.options.jsc.loose = false
+    // console.log(loader.options.jsc)
+    // config.module.rules = [
+    //     // loader,
+    //      ...config.module.rules.filter(e => !(typeof e.loader === "string" && e.loader.includes("swc-loader"))), 
+    //      loader
+    //     ]
 
     return config;
 });
