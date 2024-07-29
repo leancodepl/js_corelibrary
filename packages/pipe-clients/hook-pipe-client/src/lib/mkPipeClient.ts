37112ff6642b7125ddc938e8f8c569d897fb37ff
Aleksander Szczepura <al.szczepura@gmail.com>
2024-07-29 17:08:51 +0200

    fix: memoization

--- packages/pipe-clients/hook-pipe-client/src/lib/mkPipeClient.ts
+++ packages/pipe-clients/hook-pipe-client/src/lib/mkPipeClient.ts
@@ -1,26 +1,45 @@
-import { useEffect, useState } from "react"
+import { useEffect, useRef, useState } from "react"
 import deepEqual from "deep-equal"
+import { Observable, share } from "rxjs"
 import { NotificationsUnion, Pipe } from "@leancodepl/pipe"
 
 export function mkPipeClient({ pipe }: { pipe: Pipe }) {
     return {
         createTopic<TTopic, TNotifications extends Record<string, unknown>>(topicType: string) {
-            return (topic: TTopic) => {
+            return (topic: TTopic, { onData }: UseSubscriptionOptions<TNotifications>) => {
                 const [data, setData] = useState<NotificationsUnion<TNotifications>>()
+                const [topic$, setTopic] = useState<Observable<NotificationsUnion<TNotifications>>>()
 
-                const [internalTopic, setInternalTopic] = useState<TTopic>(topic)
-                if (!deepEqual(internalTopic, topic)) {
-                    setInternalTopic(topic)
+                const memoizedTopic = useRef<TTopic>()
+
+                if (memoizedTopic.current === undefined || !deepEqual(memoizedTopic.current, topic)) {
+                    memoizedTopic.current = topic
                 }
 
                 useEffect(() => {
-                    const subscription = pipe.topic<TNotifications>(topicType, internalTopic).subscribe(setData)
+                    const topic$ = pipe.topic<TNotifications>(topicType, memoizedTopic.current).pipe(share())
+                    setTopic(topic$)
+
+                    const subscription = topic$.subscribe(setData)
 
                     return () => subscription.unsubscribe()
-                }, [internalTopic])
+                    // eslint-disable-next-line react-hooks/exhaustive-deps
+                }, [memoizedTopic.current])
+
+                useEffect(() => {
+                    if (!topic$ || !onData) return
+
+                    const subscription = topic$.subscribe(onData)
+
+                    return () => subscription.unsubscribe()
+                }, [onData, topic$])
 
                 return { data }
             }
         },
     }
+}
+
+type UseSubscriptionOptions<TNotifications extends Record<string, unknown>> = {
+    onData?: (data: NotificationsUnion<TNotifications>) => void
 }
