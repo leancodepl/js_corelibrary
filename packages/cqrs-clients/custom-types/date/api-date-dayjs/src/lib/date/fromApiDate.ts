ffa2e7987bedd946b0b27ff6056f5263efd0759c
Łukasz Komoszyński <lukasz.komoszynski@leancode.pl>
2025-02-12 14:03:35 +0100

    feat: allow passing undefined to dayjs fromApiDate and toApiDate

--- packages/cqrs-clients/custom-types/date/api-date-dayjs/src/lib/date/fromApiDate.ts
+++ packages/cqrs-clients/custom-types/date/api-date-dayjs/src/lib/date/fromApiDate.ts
@@ -1,6 +1,12 @@
 import dayjs from "dayjs"
 import type { ApiDateOnly } from "@leancodepl/api-date"
 
-export function fromApiDate(date: ApiDateOnly): dayjs.Dayjs {
+export function fromApiDate(date: ApiDateOnly): dayjs.Dayjs
+export function fromApiDate(date: ApiDateOnly | undefined): dayjs.Dayjs | undefined
+export function fromApiDate(date: ApiDateOnly | undefined): dayjs.Dayjs | undefined {
+    if (!date) {
+        return undefined
+    }
+
     return dayjs(date as any)
 }
