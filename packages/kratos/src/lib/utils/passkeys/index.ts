8b19bd6e7969c239088889dc211b696f3d68b5a7
Kamil Golec <kumalgfilms@gmail.com>
2025-05-16 00:08:10 +0200

    feat: wrap passkey register with try-catch

--- packages/kratos/src/lib/utils/passkeys/index.ts
+++ packages/kratos/src/lib/utils/passkeys/index.ts
@@ -138,35 +138,39 @@ export async function passkeyRegister(
 
     const displayName = typeof traits?.[displayNameTraitName] === "string" ? traits[displayNameTraitName] : ""
 
-    const credential = await navigator.credentials.create({
-        signal,
-        publicKey: {
-            challenge: base64urlDecode(passkeyChallenge.publicKey.challenge),
-            timeout: passkeyChallenge.publicKey.timeout,
-            rp: {
-                id: passkeyChallenge.publicKey.rp.id,
-                name: passkeyChallenge.publicKey.rp.name,
+    try {
+        const credential = await navigator.credentials.create({
+            signal,
+            publicKey: {
+                challenge: base64urlDecode(passkeyChallenge.publicKey.challenge),
+                timeout: passkeyChallenge.publicKey.timeout,
+                rp: {
+                    id: passkeyChallenge.publicKey.rp.id,
+                    name: passkeyChallenge.publicKey.rp.name,
+                },
+                user: {
+                    id: base64urlDecode(passkeyChallenge.publicKey.user.id),
+                    name: displayName,
+                    displayName: displayName,
+                },
+                pubKeyCredParams: passkeyChallenge.publicKey.pubKeyCredParams,
             },
-            user: {
-                id: base64urlDecode(passkeyChallenge.publicKey.user.id),
-                name: displayName,
-                displayName: displayName,
+        })
+
+        if (!credential) return undefined
+        if (!(credential instanceof PublicKeyCredential)) return undefined
+        if (!(credential.response instanceof AuthenticatorAttestationResponse)) return undefined
+
+        return JSON.stringify({
+            id: credential.id,
+            rawId: base64urlEncode(credential.rawId),
+            type: credential.type,
+            response: {
+                attestationObject: base64urlEncode(credential.response.attestationObject),
+                clientDataJSON: base64urlEncode(credential.response.clientDataJSON),
             },
-            pubKeyCredParams: passkeyChallenge.publicKey.pubKeyCredParams,
-        },
-    })
-
-    if (!credential) return undefined
-    if (!(credential instanceof PublicKeyCredential)) return undefined
-    if (!(credential.response instanceof AuthenticatorAttestationResponse)) return undefined
-
-    return JSON.stringify({
-        id: credential.id,
-        rawId: base64urlEncode(credential.rawId),
-        type: credential.type,
-        response: {
-            attestationObject: base64urlEncode(credential.response.attestationObject),
-            clientDataJSON: base64urlEncode(credential.response.clientDataJSON),
-        },
-    })
+        })
+    } catch {
+        return undefined
+    }
 }
