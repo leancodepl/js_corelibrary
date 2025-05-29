bc447fe7a29489998bac2491ebc0e95231292750
Kamil Golec <kumalgfilms@gmail.com>
2025-05-30 01:41:30 +0200

    feat: add oidc form

---
+++ packages/kratos/src/lib/flows/settings/oidcForm/fields/index.ts
@@ -0,0 +1,2 @@
+export * from "./LinkOidc"
+export * from "./UnlinkOidc"
---
+++ packages/kratos/src/lib/flows/settings/passkeysForm/hooks/usePasskeys.ts
@@ -0,0 +1,63 @@
+import { useCallback, useMemo } from "react"
+import { getCsrfToken, getNodeById, inputNodeAttributes, passkeySettingsRegister } from "../../../../utils"
+import { useGetSettingsFlow, useUpdateSettingsFlow } from "../../hooks"
+
+export const usePasskeys = () => {
+    const { mutateAsync: updateSettingsFlow, isPending } = useUpdateSettingsFlow()
+    const { data: settingsFlow } = useGetSettingsFlow()
+
+    const removePasskey = useCallback(
+        async (passkeyId: string) => {
+            if (isPending) {
+                return
+            }
+
+            if (!settingsFlow) {
+                throw new Error("Settings flow is not available")
+            }
+
+            await updateSettingsFlow({
+                method: "passkey",
+                csrf_token: getCsrfToken(settingsFlow),
+                passkey_remove: passkeyId,
+            })
+        },
+        [isPending, settingsFlow, updateSettingsFlow],
+    )
+
+    const addNewPasskeyUsingCredential = useCallback(
+        async (credential: string) => {
+            if (!settingsFlow) return
+
+            await updateSettingsFlow({
+                method: "passkey",
+                csrf_token: getCsrfToken(settingsFlow),
+                passkey_settings_register: credential,
+            })
+        },
+        [settingsFlow, updateSettingsFlow],
+    )
+
+    const challenge = useMemo(
+        () => inputNodeAttributes(getNodeById(settingsFlow?.ui.nodes, "passkey_create_data")),
+        [settingsFlow?.ui.nodes],
+    )
+
+    const addNewPasskey = useCallback(async () => {
+        if (isPending) return
+
+        if (!challenge) return
+
+        const credential = await passkeySettingsRegister(challenge.value, undefined)
+
+        if (!credential) return
+
+        await addNewPasskeyUsingCredential(credential)
+    }, [isPending, challenge, addNewPasskeyUsingCredential])
+
+    return {
+        isPending,
+        removePasskey,
+        addNewPasskey,
+    }
+}
