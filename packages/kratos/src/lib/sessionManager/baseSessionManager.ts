710dc1e1936c60bc4cb8ae30808350666330ec26
Kamil Golec <kumalgfilms@gmail.com>
2025-06-17 10:08:32 +0200

    feat: remove setting returnTo url query

--- packages/kratos/src/lib/sessionManager/baseSessionManager.ts
+++ packages/kratos/src/lib/sessionManager/baseSessionManager.ts
@@ -1,6 +1,6 @@
 import { catchError, from, map, ReplaySubject, shareReplay, Subject, switchMap } from "rxjs"
 import { FrontendApi, isSessionAal2Required } from "../kratos"
-import { SearchQueryParameters, TraitsConfig } from "../utils"
+import { TraitsConfig } from "../utils"
 import { SessionWithTypedUserTraits } from "./types"
 
 export class BaseSessionManager<TTraitsConfig extends TraitsConfig> {
@@ -38,14 +38,6 @@ export class BaseSessionManager<TTraitsConfig extends TraitsConfig> {
                 switchMap(() =>
                     from(this.api.toSession()).pipe(
                         map(session => {
-                            const returnTo = new URLSearchParams(window.location.search).get(
-                                SearchQueryParameters.ReturnTo,
-                            )
-
-                            if (returnTo) {
-                                window.location.href = returnTo
-                            }
-
                             this.isAal2Required$.next(false)
 
                             return session
