85f4afb77d923902b8c3bac50a5447c99e2e9410
Kamil Golec <kumalgfilms@gmail.com>
2025-06-24 09:47:00 +0200

    feat: preserve current state when removing param from url

--- apps/example/src/utils/url.ts
+++ apps/example/src/utils/url.ts
@@ -1,7 +1,7 @@
 export const removeParamFromUrl = (param: string) => {
     const url = new URL(window.location.href)
     url.searchParams.delete(param)
-    window.history.replaceState({}, "", url.toString())
+    window.history.replaceState(window.history.state, "", url.toString())
 }
 
 export const removeFlowIdFromUrl = () => {
