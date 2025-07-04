a104847797ea6db47b51aac3f7ff712adbb5a6d2
Kamil Gacan <kamil.gacan@leancode.pl>
2025-07-04 12:30:29 +0200

    feat: add listenOnForceUpdate tests

---
+++ packages/force-update/__tests__/_utils.ts
@@ -0,0 +1,29 @@
+import { delay, of, throwError } from "rxjs"
+import { AjaxConfig, AjaxResponse } from "rxjs/ajax"
+import { ajax } from "rxjs/internal/ajax/ajax"
+
+function asMock<Func extends (...args: any[]) => any>(mockedFunc: Func) {
+    return mockedFunc as jest.MockedFunction<typeof mockedFunc>
+}
+
+// version endpoint is mocked to return next version from the array in each call
+// if all versions were used, the last version is returned indefinitely
+// if the version is null, it means that the network error occurred
+export function mockVersionEndpoint(versions: (string | null)[]) {
+    let currentVersionIndex = 0
+
+    asMock(ajax).mockImplementation((config: AjaxConfig | string) => {
+        if (typeof config === "string") throw new Error("Mock can't be used with url only")
+
+        if (config.url !== "/version") throw new Error(`${config.url} is not mocked`)
+
+        const version = versions.at(currentVersionIndex)
+        currentVersionIndex = Math.min(currentVersionIndex + 1, versions.length - 1)
+
+        if (!version) {
+            return throwError(() => new Error("Network error"))
+        }
+
+        return of({ status: 200, response: version } as AjaxResponse<string>).pipe(delay(100))
+    })
+}
---
+++ packages/force-update/__tests__/listenOnForceUpdate.spec.ts
@@ -0,0 +1,136 @@
+import { listenOnForceUpdate } from "../src"
+import { mockVersionEndpoint } from "./_utils"
+
+jest.mock("rxjs/internal/ajax/ajax")
+
+const versionCheckIntervalPeriod = 1000
+
+describe("listenOnForceUpdate", () => {
+    beforeEach(() => {
+        jest.clearAllMocks()
+        jest.useFakeTimers()
+    })
+
+    afterEach(() => {
+        jest.useRealTimers()
+    })
+
+    it("should notify when version changes", () => {
+        mockVersionEndpoint(["1.0.0", "1.0.0", "1.0.1"])
+
+        const onNewVersionAvailable = jest.fn()
+
+        const cleanup = listenOnForceUpdate({
+            onNewVersionAvailable,
+            versionCheckIntervalPeriod,
+        })
+
+        jest.advanceTimersByTime(5000)
+
+        expect(onNewVersionAvailable).toHaveBeenCalledTimes(1)
+
+        cleanup()
+    })
+
+    it("should not notify when version stays the same", () => {
+        mockVersionEndpoint(["1.0.0", "1.0.0", "1.0.0"])
+
+        const onNewVersionAvailable = jest.fn()
+
+        const cleanup = listenOnForceUpdate({
+            onNewVersionAvailable,
+            versionCheckIntervalPeriod,
+        })
+
+        jest.advanceTimersByTime(5000)
+
+        expect(onNewVersionAvailable).not.toHaveBeenCalled()
+
+        cleanup()
+    })
+
+    it("should not notify when network error occurs and initial version is fetched", () => {
+        mockVersionEndpoint(["1.0.0", "1.0.0", null])
+
+        const onNewVersionAvailable = jest.fn()
+
+        const cleanup = listenOnForceUpdate({
+            onNewVersionAvailable,
+            versionCheckIntervalPeriod,
+        })
+
+        jest.advanceTimersByTime(5000)
+
+        expect(onNewVersionAvailable).not.toHaveBeenCalled()
+
+        cleanup()
+    })
+
+    it("should notify when network errors occur, but new version is fetched eventually", () => {
+        mockVersionEndpoint(["1.0.0", null, null, "1.0.1"])
+
+        const onNewVersionAvailable = jest.fn()
+
+        const cleanup = listenOnForceUpdate({
+            onNewVersionAvailable,
+            versionCheckIntervalPeriod,
+        })
+
+        jest.advanceTimersByTime(5000)
+
+        expect(onNewVersionAvailable).toHaveBeenCalledTimes(1)
+
+        cleanup()
+    })
+
+    it("should disable force update mechanism when initial version fetch fails", () => {
+        mockVersionEndpoint([null, "1.0.0", "1.0.1"])
+
+        const onNewVersionAvailable = jest.fn()
+
+        const cleanup = listenOnForceUpdate({
+            onNewVersionAvailable,
+            versionCheckIntervalPeriod,
+        })
+
+        jest.advanceTimersByTime(5000)
+
+        expect(onNewVersionAvailable).not.toHaveBeenCalled()
+
+        cleanup()
+    })
+
+    it("should notify only once when version changes", () => {
+        mockVersionEndpoint(["1.0.0", "1.0.1", "1.0.2"])
+
+        const onNewVersionAvailable = jest.fn()
+
+        const cleanup = listenOnForceUpdate({
+            onNewVersionAvailable,
+            versionCheckIntervalPeriod,
+        })
+
+        jest.advanceTimersByTime(5000)
+
+        expect(onNewVersionAvailable).toHaveBeenCalledTimes(1)
+
+        cleanup()
+    })
+
+    it("should not notify when unsubscribed", () => {
+        mockVersionEndpoint(["1.0.0", "1.0.1"])
+
+        const onNewVersionAvailable = jest.fn()
+
+        const cleanup = listenOnForceUpdate({
+            onNewVersionAvailable,
+            versionCheckIntervalPeriod,
+        })
+
+        cleanup()
+
+        jest.advanceTimersByTime(5000)
+
+        expect(onNewVersionAvailable).not.toHaveBeenCalled()
+    })
+})
