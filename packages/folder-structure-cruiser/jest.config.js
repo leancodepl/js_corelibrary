5a7ca6a2ae91753bb7a61db6ff4798bea0d9ea0f
katarzynagrasewicz <katarzyna.grasewicz@leancode.pl>
2025-07-11 10:58:52 +0200

    chore: remove coverage

--- packages/folder-structure-cruiser/jest.config.js
+++ packages/folder-structure-cruiser/jest.config.js
@@ -1,7 +1,4 @@
 module.exports = {
     testEnvironment: "node",
     testMatch: ["**/__tests__/**/*.spec.js"],
-    collectCoverageFrom: ["src/**/*.js", "!src/**/*.test.js", "!src/**/*.spec.js"],
-    coverageDirectory: "coverage",
-    coverageReporters: ["text", "lcov"],
 }
