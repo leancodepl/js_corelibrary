9a8ce141a659532a0a284338d7bc36bd4c7d2bec
Kamil Gacan <kamil.gacan@leancode.pl>
2025-07-22 12:03:19 +0200

    chore: refactor saving outputs

--- packages/mail-translation/mail-translation.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    translationsPath: "./__tests__/translations",
-    mailsPath: "./__tests__/mails",
-    outputPath: "./dist/output",
-    outputMode: "razor",
-    defaultLanguage: "en",
-    languages: ["en", "pl"],
-}
--- packages/mail-translation/src/saveOutputs.ts
+++ packages/mail-translation/src/saveOutputs.ts
@@ -1,23 +1,15 @@
 import { mkdir, writeFile } from "fs/promises"
 import { join } from "path"
-import { OutputTemplate } from "./generateOutputTemplates"
 import { ProcessedTemplate } from "./processTemplate"
 
 export async function saveOutputs(processedTemplates: ProcessedTemplate[], outputPath: string): Promise<void> {
-    const allOutputTemplates: OutputTemplate[] = []
-    for (const processedTemplate of processedTemplates) {
-        allOutputTemplates.push(...processedTemplate.outputTemplates)
-    }
-
     await mkdir(outputPath, { recursive: true })
 
-    const outputs: Record<string, string> = {}
-    for (const template of allOutputTemplates) {
-        const filePath = join(outputPath, template.filename)
-        outputs[filePath] = template.content
-    }
+    const outputTemplates = processedTemplates.flatMap(processedTemplate => processedTemplate.outputTemplates)
 
-    await Promise.all(Object.entries(outputs).map(([file, output]) => writeFile(file, output, "utf8")))
+    await Promise.all(
+        outputTemplates.map(template => writeFile(join(outputPath, template.filename), template.content, "utf8")),
+    )
 
     for (const processedTemplate of processedTemplates) {
         if (processedTemplate.errors.length > 0) {
