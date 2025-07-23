274ac88a38245bcd31020adedecf34634a7acca3
Kamil Gacan <kamil.gacan@leancode.pl>
2025-07-23 12:09:53 +0200

    fix: include all css at rules for razor escaping

--- packages/mail-translation/src/generateRazorOutputTemplates.ts
+++ packages/mail-translation/src/generateRazorOutputTemplates.ts
@@ -14,7 +14,36 @@ export function generateRazorOutputTemplates({
 }
 
 function escapeRazorConflicts(templateContent: string): string {
-  return templateContent.replace(/(?<!@)@media/g, "@@media").replace(/(?<!@)@import/g, "@@import")
+  const cssAtRules = [
+    "annotation",
+    "character-variant",
+    "charset",
+    "color-profile",
+    "container",
+    "counter-style",
+    "font-face",
+    "font-feature-values",
+    "font-palette-values",
+    "import",
+    "keyframes",
+    "-webkit-keyframes",
+    "layer",
+    "media",
+    "namespace",
+    "ornaments",
+    "page",
+    "position-try",
+    "property",
+    "scope",
+    "starting-style",
+    "styleset",
+    "stylistic",
+    "supports",
+    "swash",
+    "view-transition",
+  ].join("|")
+
+  return templateContent.replace(new RegExp(`(?<!@)@(${cssAtRules})`, "g"), "@@$1")
 }
 
 function getFilename({
