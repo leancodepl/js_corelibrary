61399ed6f53cdbdf8f920d655aa9bedfdd917166
Kamil Gacan <kamil.gacan@leancode.pl>
2025-08-01 14:18:48 +0200

    feat: remove logging terms used locally but not in remote

--- packages/intl/src/commands/diff.ts
+++ packages/intl/src/commands/diff.ts
@@ -22,10 +22,6 @@ export async function diff({ srcPattern, translationsServiceClient }: DiffComman
     const remoteTerms = await translationsServiceClient.downloadTerms()
     const remoteTermSet = new Set(remoteTerms.map(term => term.term))
 
-    console.log("\nComparison Results:")
-    console.log(`Local terms: ${localTerms.size}`)
-    console.log(`Remote terms: ${remoteTermSet.size}`)
-
     const unusedInLocal = [...remoteTermSet].filter(term => !localTerms.has(term))
     if (unusedInLocal.length > 0) {
       console.log(`\nTerms in remote but not used locally (${unusedInLocal.length}):`)
@@ -34,17 +30,6 @@ export async function diff({ srcPattern, translationsServiceClient }: DiffComman
       })
     } else {
       console.log("\nNo unused terms found in remote")
-    }
-
-    const missingInRemote = [...localTerms].filter(term => !remoteTermSet.has(term))
-    if (missingInRemote.length > 0) {
-      console.log(`\nTerms used locally but missing in remote (${missingInRemote.length}):`)
-      missingInRemote.forEach(term => {
-        console.log(`  - ${term}`)
-      })
-      console.log("Run 'upload' command to add these terms to remote")
-    } else {
-      console.log("\nAll local terms are present in remote")
     }
   } catch (error) {
     console.error("Error in diff command:", error)
