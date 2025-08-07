5984e23a67515ff517412c962a0e7dd4fae8f1df
katarzynagrasewicz <katarzyna.grasewicz@leancode.pl>
2025-08-07 16:34:48 +0200

    feat: create resolveFlatConfig function

--- packages/linters/eslint-config/src/index.js
+++ packages/linters/eslint-config/src/index.js
@@ -2,10 +2,12 @@ const a11y = require("./lib/a11y")
 const base = require("./lib/base")
 const baseReact = require("./lib/base-react")
 const imports = require("./lib/imports")
+const resolveFlatConfig = require("./lib/resolveFlatConfig")
 
 module.exports = {
-    imports,
-    base,
-    baseReact,
-    a11y,
+  imports,
+  base,
+  baseReact,
+  a11y,
+  resolveFlatConfig,
 }
