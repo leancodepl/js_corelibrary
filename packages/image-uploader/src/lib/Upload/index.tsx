2956a51f59df99ea6d61e6002cbdee70d94b108c
Kamil Golec <kumalgfilms@gmail.com>
2025-09-02 13:20:47 +0200

    chore: remove unnecessary comments and exports

--- packages/image-uploader/src/lib/Upload/index.tsx
+++ packages/image-uploader/src/lib/Upload/index.tsx
@@ -3,7 +3,6 @@ import { UploadFiles } from "./Files"
 import { UploadRoot } from "./Root"
 import { UploadZone } from "./Zone"
 
-// Compound component with Radix-style dot notation
 export const Upload = {
   Root: UploadRoot,
   Zone: UploadZone,
@@ -11,10 +10,6 @@ export const Upload = {
   File: UploadFileItem,
 }
 
-// Export individual components for flexibility
-export { UploadFileItem, UploadFiles, UploadRoot, UploadZone }
-
-// Export types
 export type { UploadFileItemProps } from "./File"
 export type { UploadFilesProps } from "./Files"
 export type { UploadRootProps } from "./Root"
