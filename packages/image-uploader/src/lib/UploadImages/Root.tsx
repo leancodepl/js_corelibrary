b1c18e92ead329708ef61e3e899732d964d03557
Kamil Golec <kumalgfilms@gmail.com>
2025-09-04 09:05:53 +0200

    feat: simplify root component spread props

--- packages/image-uploader/src/lib/UploadImages/Root.tsx
+++ packages/image-uploader/src/lib/UploadImages/Root.tsx
@@ -1,16 +1,15 @@
-import { HTMLAttributes, ReactNode } from "react"
+import { HTMLAttributes } from "react"
 import { useUploadImages } from "../_hooks/useUploadImages"
 import { UploadImagesProvider } from "./Provider"
 
-export type UploadImagesRootProps = Omit<HTMLAttributes<HTMLDivElement>, "children"> & {
-  children: ReactNode
+export type UploadImagesRootProps = HTMLAttributes<HTMLDivElement> & {
   uploader: ReturnType<typeof useUploadImages>
 }
 
-export function UploadImagesRoot({ children, uploader, ...props }: UploadImagesRootProps) {
+export function UploadImagesRoot({ uploader, ...props }: UploadImagesRootProps) {
   return (
     <UploadImagesProvider uploader={uploader}>
-      <div {...props}>{children}</div>
+      <div {...props} />
     </UploadImagesProvider>
   )
 }
