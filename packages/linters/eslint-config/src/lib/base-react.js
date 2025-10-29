6d7850983b77db38d8b756c37caf7a1a308028b6
Aleksander Szczepura <al.szczepura@gmail.com>
2025-10-29 16:30:18 +0100

    chore: apply formatting to esling-config package

--- packages/linters/eslint-config/src/lib/base-react.js
+++ packages/linters/eslint-config/src/lib/base-react.js
@@ -3,41 +3,41 @@ const reactHooks = require("eslint-plugin-react-hooks")
 const globals = require("globals")
 
 module.exports = [
-    {
-        plugins: {
-            react,
-            "react-hooks": reactHooks,
-        },
-        languageOptions: {
-            parserOptions: {
-                ecmaFeatures: {
-                    jsx: true,
-                },
-            },
-            globals: {
-                ...globals.browser,
-            },
-        },
-        rules: {
-            "react/jsx-boolean-value": "error",
-            "react/jsx-curly-brace-presence": "warn",
-            "react/jsx-fragments": "warn",
-            "react/jsx-no-useless-fragment": ["error", { allowExpressions: true }],
-            "react/jsx-sort-props": [
-                "warn",
-                {
-                    callbacksLast: true,
-                    shorthandFirst: true,
-                    shorthandLast: false,
-                    ignoreCase: true,
-                    noSortAlphabetically: false,
-                    reservedFirst: true,
-                },
-            ],
-            "react/self-closing-comp": "error",
-
-            "react-hooks/exhaustive-deps": "error",
-            "react-hooks/rules-of-hooks": "error",
-        },
+  {
+    plugins: {
+      react,
+      "react-hooks": reactHooks,
     },
+    languageOptions: {
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+      globals: {
+        ...globals.browser,
+      },
+    },
+    rules: {
+      "react/jsx-boolean-value": "error",
+      "react/jsx-curly-brace-presence": "warn",
+      "react/jsx-fragments": "warn",
+      "react/jsx-no-useless-fragment": ["error", { allowExpressions: true }],
+      "react/jsx-sort-props": [
+        "warn",
+        {
+          callbacksLast: true,
+          shorthandFirst: true,
+          shorthandLast: false,
+          ignoreCase: true,
+          noSortAlphabetically: false,
+          reservedFirst: true,
+        },
+      ],
+      "react/self-closing-comp": "error",
+
+      "react-hooks/exhaustive-deps": "error",
+      "react-hooks/rules-of-hooks": "error",
+    },
+  },
 ]
